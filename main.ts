c0d3b9d7df95059cbb0b473f92a52ee19c659e10
M4uAWA <mau_jauregui@hotmail.com>
2025-11-26 23:29:36 -0600

    API added

--- main.ts
+++ main.ts
@@ -1,26 +1,46 @@
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js"
 import { z } from 'zod'
 
 //server instantiation
 const server = new McpServer({
-    name: "wheather-mcp-server",
-    version: "1.0.0",
+    name: "currency-mcp-server",
+    version: "1.0.0"
 });
 
 //server tool
 server.tool(
-    'get-weather',
-    'tool to get the weather for city',
+    'convert-currency',
+    'tool to convert an amount between two currencies using an open API',
     {
-        city: z.string().describe('The name of the city to get the weather for'),
+        amount: z.number().describe('Amount of money to convert'),
+        from: z.string().describe('Currency code to convert from, e.g., USD'),
+        to: z.string().describe('Currency code to convert to, e.g., EUR'),
     },
-    async ({ city }) => {
-        //Request
+    async ({ amount, from, to }) => {
+
+        // Llamada a API gratuita (Frankfurter)
+        const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`);
+
+        if (!res.ok) {
+            return {
+                content: [
+                    {
+                        type: "text",
+                        text: `Error getting conversion rate.`
+                    }
+                ]
+            }
+        }
+
+        const data = await res.json();
+        const converted = data.rates[to];
+
         return {
             content: [
                 {
                     type: "text",
-                    text: `The weather in ${city} is sunny with a high of 25°C.`
+                    text: `${amount} ${from} equals ${converted} ${to}.`
                 }
             ]
         }
@@ -28,3 +48,5 @@ server.tool(
 );
 
 //local connection
+const transport = new StdioServerTransport();
+server.connect(transport);
